e2efa789461a1da707f9f0650b82593bd5b6a09e
brami007 <brami007@fiu.edu>
2019-11-09 09:40:41 -0500

    added some clicking logic

--- src/components/playerStats.js
+++ src/components/playerStats.js
@@ -8,22 +8,28 @@ class Player extends Component {
     constructor(props){
         super(props);
           this.addOne = this.addOne.bind(this);
+          this.toggleHover = this.toggleHover.bind(this);
             this.state = {
-              count : 0 ,
+              currentMoney : 0 ,
               hovered: false
             }
          }
+
+        
+         
      
      addOne() {                             
        this.setState((preState) => {
          return {
-           count : preState.count + 1
+           currentMoney : preState.currentMoney + 1
            };
         });
       }
 
       
-  
+      toggleHover() {
+        this.setState({hovered: !this.state.hovered})
+      }
     
 
 
@@ -31,13 +37,13 @@ class Player extends Component {
       return <div class="col-md-3 p-0 back_coin">
         <div class="namePlayer">
             <p>Name Player</p>
-            <h3>{this.state.count}</h3>
+            <h3>{this.state.currentMoney}</h3>
             <h5>Per seconds 120</h5>
         </div>
         
             <div class="coin">
                 <a onClick={this.addOne}>
-                    <img onMouseDown={() => this.setState(prevState =>({hovered: !prevState.hovered}))}   style={{transform: `${this.state.hovered ? 'scale(1.5,1.5)' : null}`}} src="./images/dollar.png" alt=""/>
+                    <img onMouseEnter={this.toggleHover} onMouseLeave={this.toggleHover}   style={{transform: `${this.state.hovered ? 'scale(1.5,1.5)' : 'scale(1,1)'}`}} src="./images/dollar.png" alt=""/>
             </a>
         
         </div>
--- src/components/properties/plot.js
+++ src/components/properties/plot.js
@@ -1,34 +1,44 @@
 import React,{Component} from 'react';
+import Player from '../playerStats';
 
 
 class Plot extends Component {
+  
   constructor(props){
     super(props);
       this.addOne = this.addOne.bind(this);
         this.state = {
           count : 0 ,
-          money: 0
+          money: 10,
+          currentMoney: ''
         }
      }
 
-    addOne() {                             
+  
+    handleMoneyInputChange = ({target}) => {
+      const { value: currentMoney } = target;
+      this.setState({ currentMoney })
+    }
+
+     addOne() {                             
       this.setState((preState) => {
         return {
-          count : preState.count + 1
+          count : preState.count + 1,
+          money : preState.money * 1.05
           };
        });
      }
 
     render() {
       return  <div class="item_house_cont">
+      <a onClick={this.addOne}>
       <img src="./images/plot.png" alt=""/>
+      </a>
       <div class="date_cont">
           <h3>Plot</h3>
-          <a onClick={this.addOne}>
-          <img src="./images/dollar.png" alt=""/><p>{this.state.count}</p>
-          </a>
+          <img src="./images/dollar.png" alt=""/><p>{this.state.money}</p>
       </div>
-      <h2>32</h2>
+      <h2>{this.state.count}</h2>
   </div>
     }
   }
